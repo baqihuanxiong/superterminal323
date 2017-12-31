6c2e54ef434d7ccf411e4689412375d92cf96d6e
baqihuanxiong <lw390937635@163.com>
2017-12-31 21:08:18 +0800

    转弯模型

---
+++ agv/agv_path.m
@@ -0,0 +1,35 @@
+L = 0.16; % 前读卡器与后轮轴中心距离(m)
+v0 = 0.05; % 车速(m/s)
+beta = pi/2; % 舵机转速(rad/s)
+delt_t = 0.01;
+ 
+alpha = 0; % 起始转角
+theta = pi/2; % 车身姿态角
+xf = 0;
+yf = 0;
+xb = -1*L*cos(theta);
+yb = -1*L*sin(theta);
+
+xfs = [];
+xbs = [];
+yfs = [];
+ybs = [];
+for t=0:delt_t:10
+    xf = xf+v0*cos(theta-alpha)/cos(alpha)*delt_t;
+    yf = yf+v0*sin(theta-alpha)/cos(alpha)*delt_t;
+    xb = xb+v0*cos(theta)*delt_t;
+    yb = yb+v0*sin(theta)*delt_t;
+    theta = atan((yf-yb)/(xf-xb));
+    if beta*t<pi/6
+        alpha = beta*t;
+    else
+        alpha = pi/6;
+    end
+    xfs = [xfs xf];
+    yfs = [yfs yf];
+    xbs = [xbs xb];
+    ybs = [ybs yb];
+end
+
+plot(xfs,yfs,'r-',xbs,ybs,'b-');
+axis([-1 1 -1 1]);
